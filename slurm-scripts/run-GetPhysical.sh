cb12820f9e2d3869f009879982d587b1702486c6
lmavila <lmavila@gmail.com>
2017-06-14 15:46:38 -0700

    files deleted

--- slurm-scripts/run-GetPhysical.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-#SBATCH -D /home/lavila/projects/282inversions/
-#SBATCH -J GetPhysical
-#SBATCH -o /home/lavila/projects/282inversions/slurm-log/GetPhysical-stdout-%j.txt
-#SBATCH -e /home/lavila/projects/282inversions/slurm-log/GetPhysical-stderr-%j.txt
-~/projects/282inversions/my-scripts/get_physical.sh -s -f /home/lavila/projects/282inversions/282/c1_282_onHmp321.vcf > c1_physical.txt 
-
-#sbatch -p bigmemm -t 02:00:00  --mem 10000   slurm-scripts/run-GetPhysical.sh
-
--- slurm-scripts/run-vcfhardy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -l
-#SBATCH -D /home/lavila/projects/282inversions/WholeG
-#SBATCH -J vcfhardy
-#SBATCH -o /home/lavila/projects/282inversions/slurm-log/vcfhardy-stdout-%j.txt
-#SBATCH -e /home/lavila/projects/282inversions/slurm-log/vcfhardy-stderr-%j.txt
-vcftools --vcf all_chr_282_filtered.vcf --chr 3 --hardy
-#sbatch -p serial -t 02:00:00  --mem 500   slurm-scripts/run-vcfhardy.sh
-
